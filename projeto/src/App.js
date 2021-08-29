5efcd91e77414534acd24e6500eac2d9cbf752c7
ribeirolivia <ribeiro.livias@hotmail.com>
2021-08-29 18:17:16 -0300

    filmes add

--- projeto/src/App.js
+++ projeto/src/App.js
@@ -1,92 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
 import './App.css';
 
-export default class App extends React.Component{
-  constructor(props){
-    super(props);
-    
-    this.state = {
-      filmes: [
-        {
-          id: 1,
-          nome: "Carlota Joaquina, Princesa do Brazil",
-          imagemUrl:
-          "https://upload.wikimedia.org/wikipedia/pt/b/be/Carlota_Joaquina%2C_Princesa_do_Brazil_%281995%29_Poster.jpg",
-        },
-        {
-          id: 2,
-          nome: "Tropa de Elite",
-          imagemUrl:
-          "https://www.cafecomfilme.com.br/media/k2/items/cache/8967817feb845c0d0259582eb5885ecc_XL.jpg?t=20160828_195108",
-        },
-        {
-          id: 3,
-          nome: "Vidas Secas",
-          imagemUrl:
-          "https://upload.wikimedia.org/wikipedia/pt/thumb/7/7f/Vidas_Secas_CN_0116A.jpg/240px-Vidas_Secas_CN_0116A.jpg",
-        },
-        {
-          id: 4,
-          nome: "Do começo ao fim" ,
-          imagemUrl:
-          "https://i.pinimg.com/originals/d1/5f/9d/d15f9d9d186b314767a7c5b2b4ab268b.jpg",
-        },
-        {
-          id: 5,
-          nome: "O Auto da Compadecida",
-          imagemUrl:
-          "https://upload.wikimedia.org/wikipedia/pt/thumb/b/bf/O_auto_da_compadecida.jpg/200px-O_auto_da_compadecida.jpg" ,
-        },
-        {
-          id: 6,
-          nome: "Bacurau",
-          imagemUrl: 
-          "https://javiu.files.wordpress.com/2019/09/bac.jpg?w=800",
-        },
-        {
-          id: 7,
-          nome: "O Palhaço",
-          imagemUrl:
-          "https://images-na.ssl-images-amazon.com/images/I/81Bsh-UIYjL.jpg" ,
-        },
-        {
-          id: 8,
-          nome:"Flores Raras",
-          imagemUrl:
-          "https://upload.wikimedia.org/wikipedia/pt/a/a7/Flores_Raras_Poster.jpg",
-        },
-        {
-          id: 9,
-          nome: "Serra Pelada",
-          imagemUrl:
-          "https://filmspot.com.pt/images/filmes/posters/big/228148_pt.jpg" ,
-        },
-        {
-          id: 10,
-          nome:"Meu nome não é Johnny",
-          imagemUrl:
-          "https://br.web.img3.acsta.net/medias/nmedia/18/87/82/88/19961969.jpg",
-        },
-      ],
-    };
-  }
-  render(){
-    const { filmes } = this.state;
-    
-    return (
-      <div className="container">
-        <h1 class = "gradiente">CineBrasil</h1>
-        <ul>
-          {filmes.map((f) => (
-            <li key = {f.id}>
-              <h3>{f.nome}</h3>
-              <img src={f.imagemUrl} alt={f.nome}/>
+export default function App() {
+  const [filmes, setfilmes] = useState([
+      {
+        id: 1,
+        nome: "Carlota Joaquina, Princesa do Brazil",
+        imagemUrl:
+        "https://upload.wikimedia.org/wikipedia/pt/b/be/Carlota_Joaquina%2C_Princesa_do_Brazil_%281995%29_Poster.jpg",
+      },
+      {
+        id: 2,
+        nome: "Tropa de Elite",
+        imagemUrl:
+        "https://www.cafecomfilme.com.br/media/k2/items/cache/8967817feb845c0d0259582eb5885ecc_XL.jpg?t=20160828_195108",
+      },
+      {
+        id: 3,
+        nome: "Vidas Secas",
+        imagemUrl:
+        "https://upload.wikimedia.org/wikipedia/pt/thumb/7/7f/Vidas_Secas_CN_0116A.jpg/240px-Vidas_Secas_CN_0116A.jpg",
+      },
+      {
+        id: 4,
+        nome: "Do começo ao fim" ,
+        imagemUrl:
+        "https://i.pinimg.com/originals/d1/5f/9d/d15f9d9d186b314767a7c5b2b4ab268b.jpg",
+      },
+      {
+        id: 5,
+        nome: "O Auto da Compadecida",
+        imagemUrl:
+        "https://upload.wikimedia.org/wikipedia/pt/thumb/b/bf/O_auto_da_compadecida.jpg/200px-O_auto_da_compadecida.jpg" ,
+      },
+      {
+        id: 6,
+        nome: "Bacurau",
+        imagemUrl: 
+        "https://javiu.files.wordpress.com/2019/09/bac.jpg?w=800",
+      },
+      {
+        id: 7,
+        nome: "O Palhaço",
+        imagemUrl:
+        "https://images-na.ssl-images-amazon.com/images/I/81Bsh-UIYjL.jpg" ,
+      },
+      {
+        id: 8,
+        nome:"Flores Raras",
+        imagemUrl:
+        "https://upload.wikimedia.org/wikipedia/pt/a/a7/Flores_Raras_Poster.jpg",
+      },
+      {
+        id: 9,
+        nome: "Serra Pelada",
+        imagemUrl:
+        "https://filmspot.com.pt/images/filmes/posters/big/228148_pt.jpg" ,
+      },
+      {
+        id: 10,
+        nome:"Meu nome não é Johnny",
+        imagemUrl:
+        "https://br.web.img3.acsta.net/medias/nmedia/18/87/82/88/19961969.jpg",
+      },
+  ]);  
 
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
+
+  return (
+    <div className="container">
+      <h1 class = "gradiente">CineBrasil</h1>
+      <h3>Cadastre seu filme</h3>
+      <form>
+        <input placeholder="Digite o nome do filme" 
+        value={nomeFilme} 
+        onChange={(e) => {
+          setNomeFilme(e.target.value)
+        }}
+        type="text"/>
+        <br></br>
+        <input placeholder="Digite a URL da imagem" value={imagemUrlFilme} type="text"/>
+        <br></br>
+      </form>
+      <ul>
+        {filmes.map((f) => (
+          <li key = {f.id}>
+            <h3>{f.nome}</h3>
+            <img src={f.imagemUrl} alt={f.nome}/>
+
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
--- projeto/src/index.js
+++ projeto/src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Teste from "./App";
+
+export default function Home() {
+  return <h1>Meu primeiro React</h1>;
+}
+
+export function Texto(props) {
+  return <p>{props.texto}</p>;
+}
+
+export function Botao(props) {
+  const [botao, setBotao] = useState(0);
+
+  useEffect(() => {
+    document.title = botao;
+  }, [botao]);
+
+  return (
+    <div>
+      <p>Número de clicks:{botao}</p>
+      <button onClick={() => setBotao(botao + 1)}>{props.nome}</button>
+    </div>
+  );
+}
+
+export function Paragrafos(props) {
+  return (
+    <div>
+      <Texto texto="esse é um simples texto na tela." texto2="outra frase" />
+      <Texto texto="outro texto na tela." />
+      <Texto texto="mais um texto." />
+      <Texto texto="Ultima linha do texto." />
+    </div>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Home />
+    <Paragrafos texto2="alguma coisa"/>
+    <Botao nome="Acumular" />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
